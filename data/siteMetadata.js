77a444f2f7ae82da7f2f721a7da9ba6d7c156156
Asmita <asmitass2911@gmail.com>
2025-10-20 01:46:03 -0400

    Initial commit

---
+++ data/siteMetadata.js
@@ -0,0 +1,74 @@
+/** @type {import("pliny/config").PlinyConfig } */
+const siteMetadata = {
+  title: 'Asmita Blogs',
+  author: 'Asmita Sonavane',
+  headerTitle: 'Stacked',
+  description: 'Turning complexity into clarity, one stack at a time.',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://your-project.vercel.app',
+  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
+  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
+  email: 'as20428@nyu.edu',
+  github: 'https://github.com/Asmi2911',
+  linkedin: 'https://www.linkedin.com/in/asmita2911/',
+  instagram: 'https://www.instagram.com/asmitassonavane/',
+  siteRepo: 'https://github.com/Asmi2911',
+
+  locale: 'en-US',
+  // set to true if you want a navbar fixed to the top
+  stickyNav: false,
+  analytics: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
+    umamiAnalytics: {
+      // We use an env variable for this site to avoid other users cloning our analytics ID
+      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+      // You may also need to overwrite the script if you're storing data in the US - ex:
+      // src: 'https://us.umami.is/script.js'
+      // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
+    },
+    // plausibleAnalytics: {
+    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    // If you are hosting your own Plausible.
+    //   src: '', // e.g. https://plausible.my-domain.com/js/script.js
+    // },
+    // simpleAnalytics: {},
+    // posthogAnalytics: {
+    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
+    // googleAnalytics: {
+    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
+    // },
+  },
+  newsletter: {
+    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
+    // Please add your .env file and modify it according to your selection
+    provider: '',
+  },
+  comments: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // Select a provider and use the environment variables associated to it
+    // https://vercel.com/docs/environment-variables
+    provider: '', // supported providers: giscus, utterances, disqus
+    
+  },
+  search: {
+    provider: 'kbar', // kbar or algolia
+    kbarConfig: {
+      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
+    },
+    // provider: 'algolia',
+    // algoliaConfig: {
+    //   // The application ID provided by Algolia
+    //   appId: 'R2IYF7ETH7',
+    //   // Public API key: it is safe to commit it
+    //   apiKey: '599cec31baffa4868cae4e79f180729b',
+    //   indexName: 'docsearch',
+    // },
+  },
+}
+
+module.exports = siteMetadata
